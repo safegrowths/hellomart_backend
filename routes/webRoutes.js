c646dfc04fc5fef034391db25de2c4912b6cde1d
nitish-codes01 <madheshyanitish233227@gmail.com>
2026-04-24 01:16:04 +0530

    changed file

--- routes/webRoutes.js
+++ routes/webRoutes.js
@@ -8,7 +8,7 @@ router.get('/about', pageController.aboutPage);
 router.get('/dashboard', pageController.dashboardPage);
 router.get('/users', pageController.usersPage);
 router.get('/categoriess', pageController.categorisePage);
-router.get('/subcategories', pageController.subcategorisePage);
+router.get('/subcategoriess', pageController.subcategorisePage);
 router.get('/settings', pageController.settingsPage);
 router.get('/product-attributes', pageController.getProductAttributes);
 router.get('/coupon', pageController.couponForm);
@@ -21,10 +21,10 @@ router.get('/users_view', pageController.users_view);
 router.get('/products', pageController.productsForm);
 router.get('/transactions', pageController.transactionsForm);
 router.get('/subcategories', pageController.subcategoryview);
-router.post('/subcategories/add', pageController.add_subcategory);
-router.post('/subcategories/update', pageController.update_subcategory);
-router.get('/subcategories/delete/:id', pageController.delete_subcategory);
-router.get('/subcategories/status/:id', pageController.toggle_status);
+router.post('/subcategories_add', pageController.add_subcategory);
+router.post('/subcategories_update', pageController.update_subcategory);
+router.get('/subcategories_delete/:id', pageController.delete_subcategory);
+router.get('/subcategories_status/:id', pageController.toggle_status);
 
 
 
